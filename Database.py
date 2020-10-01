c358623a30d022dce17844e883b102083430ca8a
Nagibator1999 <uxin.nikita2016@gmail.com>
2020-10-01 22:57:28 +0300

    archive and mess

--- Database.py
+++ Database.py
@@ -1,6 +1,7 @@
 import psycopg2
 from psycopg2 import Error
 from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
+import datetime
 
 # возможные значения для action: CREATE/DROP
 def create_or_drop_database(host, user, port, password, database, action):
@@ -25,7 +26,7 @@ def create_or_drop_database(host, user, port, password, database, action):
         
 
 class Database(object):
-    def __init__(self, host = "127.0.0.1", database = "test", user = "postgres", port = 5432, password = "fgt6oij12c"):
+    def __init__(self, host = "127.0.0.1", database = "test", user = "postgres", port = 5432, password = "fgt6oij12c", archive = "archive"):
 
         self.host = host
         self.database = database
@@ -34,6 +35,7 @@ class Database(object):
         self.password = password
         self.connection = None
         self.cursor = None
+        self.archive = archive
 
     # подсоединяемся к базе данных
     # cur = True / создавать курсор или нет (по умолчанию - да)
@@ -53,19 +55,29 @@ class Database(object):
         except (Exception, psycopg2.DatabaseError) as error:
             print("Error while connection to PostgreSQL", error)
 
+    # отключаемся от базы данных
     def disconnect(self):
         self.cursor.close()
         self.connection.close()
         print("PostgreSQL connection is closed")
 
-    def create_table(self, name_of_table, columns, data_type_and_constraints):
+    # my_query указывать без скобок
+    def create_table(self, name_of_table, columns, data_type_and_constraints, my_query = ""):
         try:
             query_create_table = "CREATE TABLE {} (\n".format(name_of_table)
 
-            query_tail = ""
-            for column, dataTS in zip(columns, data_type_and_constraints):
-                query_tail += column + " " + dataTS + ",\n"
-            query_create_table += query_tail[:-2] + "\n);"
+            if (my_query == ""):
+                if (type(columns) != tuple):
+                    columns = tuple(columns)
+                if (type(data_type_and_constraints) != tuple):
+                    columns = tuple(data_type_and_constraints)
+
+                query_tail = ""
+                for column, dataTS in zip(columns, data_type_and_constraints):
+                    query_tail += column + " " + dataTS + ",\n"
+                query_create_table += query_tail[:-2] + "\n);"
+            else:
+                query_create_table += (my_query + "\n);")
 
             self.cursor.execute(query_create_table)
             self.connection.commit()
@@ -75,45 +87,26 @@ class Database(object):
         except (Exception, psycopg2.DatabaseError) as error:
             print("Error while connection to PostgreSQL", error)       
 
-    def insert(self, table, columns, values):
-        try:
-            query = "INSERT INTO {0} {1} VALUES (".format(table, columns).replace("'","")
-            query += ("%s,"*len(columns))[:-1]+")"
-            values = tuple(values)
+    # def update(self, table, rows, value_of_row, columns, values):
+    #     try: 
+    #         if (type(values) != tuple):
+    #             values = tuple(values)
+    #         if (type(value_of_row) != tuple):
+    #             value_of_row = tuple(value_of_row)
 
-            self.cursor.executemany(query, values)
-            self.connection.commit()
-            print(self.cursor.rowcount, "Record inserted successfully into {} table".format(table))
+    #         records = list()
 
-        except (Exception, psycopg2.DatabaseError) as error:
-            print("Error while connection to PostgreSQL", error)
+    #         for record, row in zip(values, value_of_row):
+    #             tmp = [record, row]
+    #             records.append(tuple(tmp))
 
-    def update(self, table, rows, value_of_row, columns, values):
-        try: 
-            query = "Update {0} set {1} = %s where {2} = %s".format(table, columns, rows)
+    #         self.cursor.executemany(query, records)
+    #         self.connection.commit()
 
-            # для архивации !!!!
+    #         print("Records Updated")
 
-            # для архивации !!!!
-
-            # если вдруг они окадутся неитерируемыми объектами
-            values = tuple(values)
-            value_of_row = tuple(value_of_row)
-
-            records = list()
-
-            for record, row in zip(values, value_of_row):
-                tmp = [record, row]
-                records.append(tuple(tmp))
-
-            print(records)
-            self.cursor.executemany(query, records)
-            self.connection.commit()
-
-            print("Records Updated")
-
-        except (Exception, psycopg2.DatabaseError) as error:
-            print("Error while connection to PostgreSQL", error)
+    #     except (Exception, psycopg2.DatabaseError) as error:
+    #         print("Error while connection to PostgreSQL", error)
 
     def select_n_first_rows(self, table, number_of_rows):
         result = list()
@@ -132,16 +125,80 @@ class Database(object):
 
         return result
 
-    def select_columns(self, table, number_of_rows, columns, where = ""):
+    def insert(self, table, columns, values):
+        try:
+            query = "INSERT INTO {0} {1} VALUES (".format(table, columns).replace("'","")
+            query += ("%s,"*len(columns))[:-1]+")"
+            values = tuple(values)
+            
+            if (type(values) == list):
+                self.cursor.executemany(query, values)
+            else:
+                self.cursor.execute(query, values)
+            self.connection.commit()
+            print(self.cursor.rowcount, "Record inserted successfully into {} table".format(table))
+
+        except (Exception, psycopg2.DatabaseError) as error:
+            print("Error while connection to PostgreSQL", error)
+
+    # выбор по стобцам
+    def select(self, table, column, condition = False):
+        result = None
+        try:
+            query = "select {0} from {1}".format(column, table)
+            if (condition):
+                query += (" where " + condition)
+            
+            self.cursor.execute(query)
+            result =  self.cursor.fetchall()            
+            
+            print("Selectring columns from table {} finished successfully".format(table))
+
+        except (Exception, psycopg2.DatabaseError) as error:
+            print("Error while connection to PostgreSQL", error)
+        finally:
+            return result
+
+    # обновляет одно значение
+    def update_one(self, table, column, another_column, id, value):
+        try: 
+            # для архивации !!!!
+            # пока что предполложим что столбцы в архиве будут такие:
+            # id | datetime | value | row | column
+
+            # проверим существует ли архив
+
+            self.cursor.execute("select exists(select * from information_schema.tables where table_name=%s)", (self.archive,))
+
+            if (not self.cursor.fetchone()[0]):
+                self.create_table( self.archive, ("id", "datetime", "value", "row", "col"), ("SERIAL PRIMARY KEY", "TIMESTAMP", "VARCHAR (100)", "VARCHAR (100)", "VARCHAR (100)"))
+            time = datetime.datetime.now()
+            print(time)
+            changed_val = self.select(table, column, "{0} = {1}".format(another_column, id))
+            self.insert(self.archive, ("datetime", "value", "row", "col"), (time, str(changed_val), another_column, column) )
+
+            query = "update {0} set {1} = {2} where {3} = {4}".format(table, column, value, another_column, id)
+
+            self.cursor.execute(query)
+            self.connection.commit()
+
+            print("Records Updated")
+
+        except (Exception, psycopg2.DatabaseError) as error:
+            print("Error while connection to PostgreSQL", error)
+
+    def select_many_columns(self, table, number_of_rows, columns, where = ""):
         result = dict()
         try:
             if type(columns) == str:
-                columns = list(columns)
+                tmp = list()
+                tmp.append(columns)
+                columns = tmp
 
             for column in columns:
                 select_query = "SELECT {0} FROM {1}".format(column,table)
                 if (where != ""):
-                    select_query += " where {}".format(where)
+                    select_query += (" " + where)
                 self.cursor.execute(select_query)
                 if number_of_rows == "ALL":
                     fatched = self.cursor.fetchall()
@@ -174,4 +231,10 @@ class Database(object):
 # затем закрываем соединение (a.disconnect())
 
 if __name__ == "__main__":
-    pass
+    a = Database(database="to_delete")
+    a.connect()
+    # a.update("table_with_phones", "id", (1,2), "price", (3000, 450))
+    a.update_one("table_with_phones", "price", "model", "'Xiaomi'", 120)
+    something = a.select("table_with_phones", "price", "id < 6")
+    print(something)
+    a.disconnect()
