cd9ff518dc4f78df9af1186fbf9afb8c61f0087c
Nagibator1999 <uxin.nikita2016@gmail.com>
2020-10-05 12:35:08 +0300

    init function

--- db.py
+++ db.py
@@ -17,6 +17,8 @@ class BaseModel(Model):
         database = pg_db
 
 # след. три таблицы для листов из Сигналы.xlsx 
+# важная ремарочка: в PostgreSQL имена колонок xmin, xmax
+# являются зарезервированными и их нельзя использователь в таблице
 class MPK(BaseModel):
     id = AutoField(primary_key = True)
     id_kks = IntegerField(null = True)
@@ -42,43 +44,43 @@ class MPK(BaseModel):
 
 class RS485(BaseModel):
     id = AutoField(primary_key = True)
-    kks = CharField(null = True)
+    KKS = CharField(null = True)
     Суффикс = CharField(null = True)
     Модуль = CharField(null = True)
     Слот = IntegerField(null = True)
     Канал = IntegerField(null = True)
-    name = CharField(null = True)
-    xmin_ = FloatField(null = True)
-    xmax_ = FloatField(null = True)
+    NAME = CharField(null = True)
+    Xmin_ = FloatField(null = True)
+    Xmax_ = FloatField(null = True)
     unit = CharField(null = True)
-    la = FloatField(null = True)
-    lw = FloatField(null = True)
-    hw = FloatField(null = True)
-    ha = FloatField(null = True)
+    LA = FloatField(null = True)
+    LW = FloatField(null = True)
+    HW = FloatField(null = True)
+    HA = FloatField(null = True)
     Точность_лог = FloatField(null = True)
     Точность_вк = FloatField(null = True)
-    type = CharField(null = True)
+    Type = CharField(null = True)
 
     class Meta:
         table_name = "RS458"
 
 class Calculated(BaseModel):
     id = AutoField(primary_key = True)
-    kks = CharField(null = True)
+    KKS = CharField(null = True)
     Суффикс = CharField(null = True)
     Модуль = CharField(null = True)
     Слот = IntegerField(null = True)
     Канал = IntegerField(null = True)
     name = CharField(null = True)
-    xmin_ = FloatField(null = True)
-    xmax_ = FloatField(null = True)
+    Xmin_ = FloatField(null = True)
+    Xmax_ = FloatField(null = True)
     unit = CharField(null = True)
-    type = CharField(null = True)
+    Type = CharField(null = True)
     Formula = FloatField(null = True)
-    la = FloatField(null = True)
-    lw = FloatField(null = True)
-    hw = FloatField(null = True)
-    ha = FloatField(null = True)
+    LA = FloatField(null = True)
+    LW = FloatField(null = True)
+    HW = FloatField(null = True)
+    HA = FloatField(null = True)
     Точность_лог = FloatField(null = True)
     Точность_вк = FloatField(null = True)
 
@@ -100,22 +102,35 @@ class Archive(BaseModel):
 
 def create_tables():
     with pg_db:
-        pg_db.create_tables([MPK])
+        pg_db.create_tables([MPK, Archive])
+
+def drop_tables():
+    with pg_db:
+        pg_db.drop_tables([MPK, Archive])
 
 # функция для инициализации таблицы
 def init():
-    # важная ремарочка: в PostgreSQL имена колонок xmin, xmax
-    # являются зарезервированными и их нельзя использователь в таблице
-
     create_tables()
 
     # заполняем таблцу МПК
     df = pd.read_excel('Сигналы.xlsx', sheet_name='МПК')
     data = df.to_dict(orient = 'records')
-    print(data[0])
+
     with pg_db.atomic():
         MPK.insert_many(data).execute()
-    print("Success")
+    print("MPK filled successfully")
+
+    # заполняем таблицу Archive
+    df = pd.read_csv('arhive.csv')
+    data = df.to_dict(orient = 'records')
+    
+    # кто-то придумал хранить дату как строки, поэтому сделаем эти строки обратно датой
+    for index in range(len(data)):
+        data[index]['session_date'] = pd.to_datetime(data[index]['session_date'])
+
+    with pg_db.atomic():
+        Archive.insert_many(data).execute()
+    print("Archive filled successfully")
 
 if __name__ == '__main__':
-    init()
+    # init()
